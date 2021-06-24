35378a94467559a25e5a372566d00344e8bc6df7
anuradha <anuradhayadav691@gmail.com>
2021-06-24 09:20:04 +0530

    Add program area & checkelememt

---
+++ area.rb
@@ -0,0 +1,9 @@
+puts 'enter length'
+
+l = gets.chomp.to_f
+puts 'enter width'
+
+w = gets.chomp.to_f
+area = l * w
+
+puts "Area is #{area}" 
---
+++ checkelememt.rb
@@ -0,0 +1,13 @@
+arr = Array['Nodejs', 'Java Script', 'Ruby', 'Python']
+
+puts 'Enter the element you want to check'
+ele = gets.chomp
+
+check = arr.include?ele   
+if check == true
+  puts "#{ele} is an element of Array"
+
+else
+  puts "#{ele} is not an element of Array"
+
+end
